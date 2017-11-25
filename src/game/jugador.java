95c79ef10a14e2f88a41c66327da52039b683751
Ana Maria Sativa <anamaria.sativa@enerwygroup.com>
2017-11-25 11:21:14 -0500

    Face 3.1

---
+++ src/game/jugador.java
@@ -0,0 +1,21 @@
+package game;
+
+public class jugador {
+	public String nombre;
+
+	public jugador(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
